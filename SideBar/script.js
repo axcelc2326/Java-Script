9063578944e9f84c6cf8655832b3b12ceba746ac
axcelc2326 <axcelc2326@gmail.com>
2024-11-13 09:56:47 +0800

    Re-styling

--- SideBar/script.js
+++ SideBar/script.js
@@ -1,6 +1,10 @@
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
-    const content = document.querySelector('.content');
-    sidebar.classList.toggle('open');
-    content.classList.toggle('shifted'); 
+    const currentLeft = sidebar.style.left;
+
+    if (currentLeft === '0px') {
+        sidebar.style.left = '-250px';
+    } else {
+        sidebar.style.left = '0px'; 
+    }
 }
